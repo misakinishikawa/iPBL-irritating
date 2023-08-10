48983fb8b98fc79503193e56cb780d590d4e95e4
misakinishikawa <141618247+misakinishikawa@users.noreply.github.com>
2023-08-10 21:33:54 +0900

    add broker_view.dart functions

--- lib/views/broker/broker_view.dart
+++ lib/views/broker/broker_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 
 import '../../constants/app.dart';
 
-class BrokerView extends StatelessWidget {
+class BrokerView extends HookWidget {
   const BrokerView({super.key});
 
   void showSnackBar(BuildContext context, String message,
@@ -19,6 +19,63 @@ class BrokerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final serverController = useTextEditingController();
+    final portController = useTextEditingController();
+    final usernameController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final clientIdController = useTextEditingController();
+
+    const defaultPort = 1883;
+
+    final server = useRef('');
+    final port = useRef(defaultPort);
+    final username = useRef('');
+    final password = useRef('');
+    final clientId = useRef('');
+
+    useEffect(() {
+      serverController.addListener(() {
+        server.value = serverController.text;
+      } []);
+
+      portController.text = defaultPort.toString();
+      portController.addListener(() {
+        port.value = int.tryParse(portController.text) ?? defaultPort;
+      } []);
+      usernameController.addListener(() {
+        username.value = usernameController.text;
+      } []);
+      passwordController.addListener(() {
+        password.value = passwordController.text;
+      } []);
+      clientIdController.addListener(() {
+        clientId.value = clientIdController.text;
+      } []);
+      return null;
+    } []);
+
+    void handleMqttClientConnect(){
+      if (server.value.isEmpty){
+        showSnackBar(context. 'Server is required');
+        return;
+      }
+
+      if(clientId.value.isEmpty){
+        var random =Random();
+        clientId.value = 'flutter_client_${random.nextInt(100)}';
+      }
+
+      final mqttManager = MqttClientManager(
+        server: server.value,
+        port: port.value,
+        username: username.value,
+        password: password.value,
+        clientId: clientId.value,
+      );
+      mqttManager.connect();
+    }
+
+
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -27,13 +84,15 @@ class BrokerView extends StatelessWidget {
             const Icon(Icons.cloud_off_outlined, size: 150, color: Colors.teal),
             const SizedBox(height: 16),
             const TextField(
-              decoration: InputDecoration(
+              controller: serverController,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: kStrServer,
               ),
             ),
             const SizedBox(height: 16),
             TextField(
+              controller:portController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: kStrPort,
@@ -43,14 +102,16 @@ class BrokerView extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const TextField(
-              decoration: InputDecoration(
+              controller: usernameController,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: kStrUsername,
               ),
             ),
             const SizedBox(height: 16),
             const TextField(
-              decoration: InputDecoration(
+              controller: passwordController,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: kStrPassword,
               ),
@@ -58,14 +119,15 @@ class BrokerView extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const TextField(
-              decoration: InputDecoration(
+              controller: clientIdController,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: kStrClientId,
               ),
             ),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () => showSnackBar(context, 'Connecting to broker...'),
+              onPressed: handleMqttClientConnect,
               style: ButtonStyle(
                 padding: MaterialStateProperty.all(
                   const EdgeInsets.symmetric(
